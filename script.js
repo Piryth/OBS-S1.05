7984b66ffb7f47d319f1a04119e57cc02913da88
STR <44617530+STRxAE@users.noreply.github.com>
2022-01-14 09:00:43 +0100

    script update

--- script.js
+++ script.js
@@ -1,30 +1,200 @@
-// Présentation de l'activité
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".example");
-var closeButton = document.querySelector(".close-button");
+//////////////////////////////////
+/// Présentation de l'activité ///
+//////////////////////////////////
 
+// Déclaration des variables
+var modal = document.getElementById("modal1");
+var trigger = document.getElementById("obutton1");
+var closeButton = document.getElementById("cbutton1");
+
+// Action en cas de clic sur le bouton
 function toggleModal() {
     modal.classList.toggle("test");
     document.body.classList.toggle("scroll")
 }
 
+// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
 function windowOnClick(event) {
     if (event.target == modal) {
         toggleModal();
     }
 }
 
+// Définition des évènements
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal)
-window.addEventListener("click", windowOnClick); 
-// Nos services
+window.addEventListener("click", windowOnClick);
 
-// Nos valeurs
 
-// Nos engagements pour le climat
 
-// Nos clients
+////////////////////
+/// Nos services ///
+////////////////////
 
-// Financement
+// Déclaration des variables
+var modal = document.getElementById("modal2");
+var trigger = document.getElementById("obutton2");
+var closeButton = document.getElementById("cbutton2");
 
-// FAQ
+// Action en cas de clic sur le bouton
+function toggleModal() {
+    modal.classList.toggle("test");
+    document.body.classList.toggle("scroll")
+}
+
+// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
+function windowOnClick(event) {
+    if (event.target == modal) {
+        toggleModal();
+    }
+}
+
+// Définition des évènements
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick);
+
+
+
+///////////////////
+/// Nos valeurs ///
+///////////////////
+
+// Déclaration des variables
+var modal = document.getElementById("modal3");
+var trigger = document.getElementById("obutton3");
+var closeButton = document.getElementById("cbutton3");
+
+// Action en cas de clic sur le bouton
+function toggleModal() {
+    modal.classList.toggle("test");
+    document.body.classList.toggle("scroll")
+}
+
+// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
+function windowOnClick(event) {
+    if (event.target == modal) {
+        toggleModal();
+    }
+}
+
+// Définition des évènements
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick);
+
+
+
+//////////////////////////////////////
+/// Nos engagements pour le climat ///
+//////////////////////////////////////
+
+// Déclaration des variables
+var modal = document.getElementById("modal4");
+var trigger = document.getElementById("obutton4");
+var closeButton = document.getElementById("cbutton4");
+
+// Action en cas de clic sur le bouton
+function toggleModal() {
+    modal.classList.toggle("test");
+    document.body.classList.toggle("scroll")
+}
+
+// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
+function windowOnClick(event) {
+    if (event.target == modal) {
+        toggleModal();
+    }
+}
+
+// Définition des évènements
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick);
+
+
+
+///////////////////
+/// Nos clients ///
+///////////////////
+
+// Déclaration des variables
+var modal = document.getElementById("modal5");
+var trigger = document.getElementById("obutton5");
+var closeButton = document.getElementById("cbutton5");
+
+// Action en cas de clic sur le bouton
+function toggleModal() {
+    modal.classList.toggle("test");
+    document.body.classList.toggle("scroll")
+}
+
+// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
+function windowOnClick(event) {
+    if (event.target == modal) {
+        toggleModal();
+    }
+}
+
+// Définition des évènements
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick);
+
+
+
+///////////////////
+/// Financement ///
+///////////////////
+
+// Déclaration des variables
+var modal = document.getElementById("modal6");
+var trigger = document.getElementById("obutton6");
+var closeButton = document.getElementById("cbutton6");
+
+// Action en cas de clic sur le bouton
+function toggleModal() {
+    modal.classList.toggle("test");
+    document.body.classList.toggle("scroll")
+}
+
+// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
+function windowOnClick(event) {
+    if (event.target == modal) {
+        toggleModal();
+    }
+}
+
+// Définition des évènements
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick);
+
+
+
+///////////
+/// FAQ ///
+///////////
+
+// Déclaration des variables
+var modal = document.getElementById("modal7");
+var trigger = document.getElementById("obutton7");
+var closeButton = document.getElementById("cbutton7");
+
+// Action en cas de clic sur le bouton
+function toggleModal() {
+    modal.classList.toggle("test");
+    document.body.classList.toggle("scroll")
+}
+
+// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
+function windowOnClick(event) {
+    if (event.target == modal) {
+        toggleModal();
+    }
+}
+
+// Définition des évènements
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal)
+window.addEventListener("click", windowOnClick);
