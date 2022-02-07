e87e8c754a010cbdf891e7964ed61e5806357d61
STR <44617530+STRxAE@users.noreply.github.com>
2022-02-07 21:43:29 +0100

    script update

--- script.js
+++ script.js
@@ -1,200 +1,40 @@
-//////////////////////////////////
-/// Présentation de l'activité ///
-//////////////////////////////////
+// Récupération des boutons qui ouvrent les fenêtres modales
+var obuttons = document.querySelectorAll(".obutton");
 
-// Déclaration des variables
-var modal1 = document.getElementById("modal1");
-var trigger1 = document.getElementById("obutton1");
-var closeButton1 = document.getElementById("cbutton1");
+// Récupération des fenêtres modales
+var modals = document.querySelectorAll('.modal');
 
-// Action en cas de clic sur le bouton
-function toggleModal() {
-    modal1.classList.toggle("openmodal");
-    document.body.classList.toggle("scroll");
+// Récupération des boutons qui ferment les fenêtres modales
+var cbuttons = document.querySelectorAll(".cbutton");
+
+// Quand l'utilisateur clique sur le bouton qui ouvre la fenêtre modale
+for (var i = 0; i < obuttons.length; i++) {
+ obuttons[i].onclick = function(e) {
+    e.preventDefault();
+    var modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden";
+
+ }
 }
 
-// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
-function windowOnClick(event) {
-    if (event.target == modal1) {
-        toggleModal();
+// Quand l'utilisateur clique sur le bouton qui ferme la fenêtre modale
+for (var i = 0; i < cbuttons.length; i++) {
+ cbuttons[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        document.body.style.overflow = "visible";
+    }
+ }
+}
+
+// Quand l'utilisateur clique à côté de la fenêtre modale
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        document.body.style.overflow = "visible";
+
+     }
     }
 }
-
-// Définition des évènements
-trigger1.addEventListener("click", toggleModal);
-closeButton1.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
-
-
-////////////////////
-/// Nos services ///
-////////////////////
-
-// Déclaration des variables
-var modal2 = document.getElementById("modal2");
-var trigger2 = document.getElementById("obutton2");
-var closeButton2 = document.getElementById("cbutton2");
-
-// Action en cas de clic sur le bouton
-function toggleModal() {
-    modal2.classList.toggle("openmodal");
-    document.body.classList.toggle("scroll");
-}
-
-// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
-function windowOnClick(event) {
-    if (event.target == modal2) {
-        toggleModal();
-    }
-}
-
-// Définition des évènements
-trigger2.addEventListener("click", toggleModal);
-closeButton2.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
-
-
-///////////////////
-/// Nos valeurs ///
-///////////////////
-
-// Déclaration des variables
-var modal3 = document.getElementById("modal3");
-var trigger3 = document.getElementById("obutton3");
-var closeButton3 = document.getElementById("cbutton3");
-
-// Action en cas de clic sur le bouton
-function toggleModal() {
-    modal3.classList.toggle("openmodal");
-    document.body.classList.toggle("scroll");
-}
-
-// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
-function windowOnClick(event) {
-    if (event.target == modal3) {
-        toggleModal();
-    }
-}
-
-// Définition des évènements
-trigger3.addEventListener("click", toggleModal);
-closeButton3.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
-
-
-//////////////////////////////////////
-/// Nos engagements pour le climat ///
-//////////////////////////////////////
-
-// Déclaration des variables
-var modal4 = document.getElementById("modal4");
-var trigger4 = document.getElementById("obutton4");
-var closeButton4 = document.getElementById("cbutton4");
-
-// Action en cas de clic sur le bouton
-function toggleModal() {
-    modal4.classList.toggle("openmodal");
-    document.body.classList.toggle("scroll");
-}
-
-// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
-function windowOnClick(event) {
-    if (event.target == modal4) {
-        toggleModal();
-    }
-}
-
-// Définition des évènements
-trigger4.addEventListener("click", toggleModal);
-closeButton4.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
-
-
-///////////////////
-/// Nos clients ///
-///////////////////
-
-// Déclaration des variables
-var modal5 = document.getElementById("modal5");
-var trigger5 = document.getElementById("obutton5");
-var closeButton5 = document.getElementById("cbutton5");
-
-// Action en cas de clic sur le bouton
-function toggleModal() {
-    modal5.classList.toggle("openmodal");
-    document.body.classList.toggle("scroll");
-}
-
-// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
-function windowOnClick(event) {
-    if (event.target == modal5) {
-        toggleModal();
-    }
-}
-
-// Définition des évènements
-trigger5.addEventListener("click", toggleModal);
-closeButton5.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
-
-
-///////////////////
-/// Financement ///
-///////////////////
-
-// Déclaration des variables
-var modal6 = document.getElementById("modal6");
-var trigger6 = document.getElementById("obutton6");
-var closeButton6 = document.getElementById("cbutton6");
-
-// Action en cas de clic sur le bouton
-function toggleModal() {
-    modal6.classList.toggle("openmodal");
-    document.body.classList.toggle("scroll");
-}
-
-// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
-function windowOnClick(event) {
-    if (event.target == modal6) {
-        toggleModal();
-    }
-}
-
-// Définition des évènements
-trigger6.addEventListener("click", toggleModal);
-closeButton6.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
-
-
-///////////
-/// FAQ ///
-///////////
-
-// Déclaration des variables
-var modal7 = document.getElementById("modal7");
-var trigger7 = document.getElementById("obutton7");
-var closeButton7 = document.getElementById("cbutton7");
-
-// Action en cas de clic sur le bouton
-function toggleModal() {
-    modal7.classList.toggle("openmodal");
-    document.body.classList.toggle("scroll");
-}
-
-// Action si fenêtre modale ouverte et clic en dehors de la fenêtre modale
-function windowOnClick(event) {
-    if (event.target == modal7) {
-        toggleModal();
-    }
-}
-
-// Définition des évènements
-trigger7.addEventListener("click", toggleModal);
-closeButton7.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
